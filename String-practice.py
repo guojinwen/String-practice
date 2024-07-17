d1f2140047baa1b8fb1004ba5ba9f4450405a940
guojinwen <38722618+guojinwen@users.noreply.github.com>
2024-07-18 07:55:37 +0800

    Create String-practice.py

---
+++ String-practice.py
@@ -0,0 +1,13 @@
+import re
+
+def input_string(string):
+  string = re.sub('[a-zA-Z]','',string)   # deleted letter
+  number = float(string) * 1.00   # convert to float type, get decima
+  number_int, number_float = str(number).split('.')   #split by '.', int & float
+  number_float = '{:0<2}'.format(number_float)  # get 2 decima
+  number = number_int + '.' + number_float[0:2]
+  return print(number)
+
+
+  # test 
+  unput_string('abc123.456')
